043f227c4bed5e414a04c1fa94060e809977e1da
Mathias Fredriksson <mafredri@gmail.com>
2022-10-31 18:04:47 +0200

    Add /api/v2/updatecheck to noauthorize list

--- coderd/coderdtest/authorize.go
+++ coderd/coderdtest/authorize.go
@@ -39,6 +39,7 @@ func AGPLRoutes(a *AuthTester) (map[string]string, map[string]RouteCheck) {
 		// These endpoints do not require auth
 		"GET:/api/v2":                   {NoAuthorize: true},
 		"GET:/api/v2/buildinfo":         {NoAuthorize: true},
+		"GET:/api/v2/updatecheck":       {NoAuthorize: true},
 		"GET:/api/v2/users/first":       {NoAuthorize: true},
 		"POST:/api/v2/users/first":      {NoAuthorize: true},
 		"POST:/api/v2/users/login":      {NoAuthorize: true},
